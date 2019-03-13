37a450a9fb654c907520d3b6566df108270806d0
Bunnbuns <ben@bunnbuns.net>
2019-03-13 16:35:53 -0400

    added settings modal and dropdown

--- assets/scripts/global.js
+++ assets/scripts/global.js
@@ -33,3 +33,39 @@ function getAllUrlParams(url) {
 	}
 	return obj;
 }
+// settings
+var saveclass = null;
+
+function saveSettings(selectID){ // --- get value from id and save it as cookie with setCookie
+    var sel = document.getElementById(selectID);
+    
+    setCookie('imageProxy', sel.value , 365);
+    $('#settingsModal').modal('hide');
+}
+function setCookie(cookieName, cookieValue, nDays) { // --- set cookie for how many days --- //
+    // credit to https://stackoverflow.com/questions/10957926/saving-dropdown-menu-selection-in-a-cookie
+    var today = new Date();
+    var expire = new Date();
+
+    if (nDays==null || nDays==0)
+        nDays=1;
+
+    expire.setTime(today.getTime() + 3600000*24*nDays);
+    document.cookie = cookieName+"="+escape(cookieValue) + ";expires="+expire.toGMTString();
+}
+function readCookie(name) { // --- return data from cookie --- //
+  // credit to https://stackoverflow.com/questions/10957926/saving-dropdown-menu-selection-in-a-cookie
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}  
+//tv_list.js
+var imageProxy = readCookie('imageProxy');
+if(imageProxy == null){
+    imageProxy = 'ON';
+}
